2bc4fb03018b3d3ef64070a80c5c7d54fb6bb887
dmitriy30011997 <112504672+dmitriy30011997@users.noreply.github.com>
2023-11-07 11:35:58 +0300

    repository corrected

--- src/main/java/DictionaryRepository.java
+++ src/main/java/DictionaryRepository.java
@@ -1,70 +1,25 @@
-import java.util.HashMap;
 import java.util.Map;
 
-public class DictionaryRepository implements Service {
+public class DictionaryRepository {
     private Map<String, String> dictionary;
 
-    public DictionaryRepository() {
-        this.dictionary = new HashMap<>();
-    }
-    public void addEntry(String key, String value, int language) {
-        if (language == 1) {
-            if (key.matches("^[a-zA-Z]{4}$") && value.matches("^[a-zA-Z]{4}$")) {
-                dictionary1.put(key, value);
-            } else {
-                System.out.println("Неверный формат слова для первого словаря.");
-            }
-        } else if (language == 2) {
-            if (key.matches("^\\d{5}$") && value.matches("^\\d{5}$")) {
-                dictionary2.put(key, value);
-            } else {
-                System.out.println("Неверный формат слова для второго словаря.");
-            }
-        } else {
-            System.out.println("Неверный выбор языка.");
-        }
+    public DictionaryRepository(Map<String, String> initialData) {
+        this.dictionary = initialData;
     }
 
-    public void deleteEntry(String key, int language) {
-        if (language == 1) {
-            dictionary1.remove(key);
-        } else if (language == 2) {
-            dictionary2.remove(key);
-        } else {
-            System.out.println("Неверный выбор языка.");
-        }
+    public void addEntry(String key, String value) {
+        dictionary.put(key, value);
     }
 
-    public String findEntry(String key, int language) {
-        if (language == 1) {
-            return dictionary1.get(key);
-        } else if (language == 2) {
-            return dictionary2.get(key);
-        } else {
-            System.out.println("Неверный выбор языка.");
-            return null;
-        }
+    public void deleteEntry(String key) {
+        dictionary.remove(key);
     }
 
-    public void setDictionary1(Map<String, String> dictionary1) {
-        this.dictionary1 = dictionary1;
-    }
-
-    public void setDictionary2(Map<String, String> dictionary2) {
-        this.dictionary2 = dictionary2;
+    public String findEntry(String key) {
+        return dictionary.get(key);
     }
 
     public Map<String, String> getDictionary() {
         return dictionary;
-    }
-
-    @Override
-    public void add(String key, String value) {
-
-    }
-
-    @Override
-    public void delete(String key, int language) {
-
     }
 }
